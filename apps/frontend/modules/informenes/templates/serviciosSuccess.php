c94fb27576e126fdb3b8d841068b498423715cc1
Luis Eloy Hernández Torrealba <luelher@gmail.com>
2010-08-14 00:31:13 -0430

    Cambios varios

--- apps/frontend/modules/informenes/templates/serviciosSuccess.php
+++ apps/frontend/modules/informenes/templates/serviciosSuccess.php
@@ -1,9 +1,14 @@
 <!-- apps/frontend/modules/job/templates/_form.php -->
 <?php use_stylesheets_for_form($form) ?>
 <?php use_javascripts_for_form($form) ?>
+
+<div>
+  <h1>Informe de Servicios</h1>
+</div>
+
 <div>
 <?php echo form_tag('informenes/servicios') ?>
       <?php echo $form ?>
-  <input type="submit" name="Enviar">
+  <input type="submit" name="generar">
 </form>
 </div>
