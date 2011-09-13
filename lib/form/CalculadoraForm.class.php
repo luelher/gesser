77686f56b869c887f24df2ef50e3c40a83f6b26d
root <root@server05.agenciaroyal>
2011-09-13 13:26:58 -0430

    cambio en el formulario del calculo

--- lib/form/CalculadoraForm.class.php
+++ lib/form/CalculadoraForm.class.php
@@ -9,14 +9,14 @@ class CalculadoraForm extends BaseForm
   {
     $this->setWidgets(array(
       'monto_venta'    => new sfWidgetFormInputText(array(),array('class' => 'clearfix')),
-      'porcentaje_inicial' => new sfWidgetFormInputText(array(),array('class' => 'clearfix')),
+      'porcentaje_inicial' => new sfWidgetFormInputText(array('default' => 0),array('class' => 'clearfix')),
       'cuotas'   => new sfWidgetFormInputText(array(),array('class' => 'clearfix')),
       'descuento' => new sfWidgetFormInputText(array('default' => 0),array('class' => 'clearfix')),
     ));
 
     $this->setValidators(array(
-      'monto_venta' => new sfValidatorNumber(array('min' => 0, 'max' => 999999999, 'required' => true), array('required' => 'Debe agregar el monto de la venta', 'min' =>'El número debe ser mayor a 10, 'max' => 'El número debe ser menor a 999.999.999')),
-      'porcentaje_inicial'  => new sfValidatorNumber(array('min' => 1, 'max' => 999999999, 'required' => true), array('required' => 'Debe agregar el porcentaje a aplicar', 'min' =>'El número debe ser mayor a 1', 'max' => 'El número debe ser menor a 999.999.999')),
+      'monto_venta' => new sfValidatorNumber(array('min' => 1, 'max' => 999999999, 'required' => true), array('required' => 'Debe agregar el monto de la venta', 'min' =>'El número debe ser mayor a 1, 'max' => 'El número debe ser menor a 999.999.999')),
+      'porcentaje_inicial'  => new sfValidatorNumber(array('min' => 0, 'max' => 999999999, 'required' => true), array('required' => 'Debe agregar el porcentaje a aplicar', 'min' =>'El número debe ser mayor a  0', 'max' => 'El número debe ser menor a 999.999.999')),
       'cuotas'      => new sfValidatorNumber(array('min' => 1, 'max' => 999999999, 'required' => true), array('required' => 'Debe agregar la cantidad de cuotas', 'min' =>'El número debe ser mayor a 1', 'max' => 'El número debe ser menor a 999.999.999')),
       'descuento'   => new sfValidatorNumber(array('min' => 0, 'max' => 999999999, 'required' => true), array('required' => 'Debe agregar el monto de descuento', 'min' =>'El número debe ser mayor a 1', 'max' => 'El número debe ser menor a 999.999.999')),
     ));
